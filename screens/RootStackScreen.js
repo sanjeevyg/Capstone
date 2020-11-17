a78741b323ca3cfa1cd8cbf705c5d4b9df5b7385
sanjeevyg <yogi.sjv@gmail.com>
2020-11-17 16:22:08 -0700

    root stack updated

--- screens/RootStackScreen.js
+++ screens/RootStackScreen.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-// import { View, Text, Button, StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SplashScreen from './SplashScreen';
